418f86492b73ab656b796671805e31d12c427a82
wdrayton <wdrayton@live.unc.edu>
2023-02-02 14:51:01 -0500

    Fourth submission

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 // Require http module
-const http = Require('http');
+const http = require('http');
 // Require fs module
-const fs = Require('fs');
+const fs = require('fs');
 // Require minimist module (make sure you install this one via npm).
-const minimist = Require('minimist');
+var minimist = require('minimist');
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
 const args = minimist(process.argv.slice(2))
 // Define a const `port` using the argument from the command line. 
