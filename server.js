0246383575de4d61e75a6c1a12290a5a55987c01
wdrayton <wdrayton@live.unc.edu>
2023-02-03 10:59:47 -0500

    Tenth commit

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ fs.readFile('./public/index.html', (err, data) => {
 // 3. end with the data that you are reading in from ./public/index.html.
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/header');
+    res.setHeader('Content-Type', 'text/html');
     res.end(index);
 });
 // Start the `server` const listening on the port defined by argument in your `port` const. 
